1c93617df763d6a5e64bfa553873d1f2b433441a
Kristin Galvin <kgalvin@nvidia.com>
2022-08-10 11:11:23 -0400

    improve audio context control

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 import { el } from "@elemaudio/core";
 import type { NodeRepr_t } from "@elemaudio/core";
 import WebRenderer from "@elemaudio/web-renderer";
 
 function App() {
+  const [playing, setPlaying] = useState(false);
   const [steps, setSteps] = useState<number>(4);
   const [modAmp, setModAmp] = useState<number>(10000);
   const [startAmp, setStartAmp] = useState<number>(20000);
   const [startFreq, setStartFreq] = useState<number>(0.01);
   const [modAmpMult, setModAmpMult] = useState<number>(2);
   const [ampLimit, setAmpLimit] = useState<number>(1);
+  const audioContextRef = useRef<AudioContext | null>(null);
 
-  let recursiveFM = (t: NodeRepr_t, amp: number, steps: number): NodeRepr_t => {
-    return steps > 0 && amp > ampLimit
-      ? recursiveFM(el.cycle(el.mul(t, amp)), amp / modAmpMult, steps - 1)
-      : t;
-  };
+  useEffect(() => {
+    const audioContext = new AudioContext({
+      latencyHint: "interactive",
+      sampleRate: 44100,
+    });
 
-  function playAudio() {
-    const ctx = new AudioContext();
+    audioContextRef.current = audioContext;
+    audioContext.suspend();
+  }, []);
+
+  const recursiveFM = useCallback(
+    (t: NodeRepr_t, amp: number, counter: number): NodeRepr_t => {
+      console.log("recursive counter is ", counter);
+      return counter > 0 && amp > ampLimit
+        ? recursiveFM(el.cycle(el.mul(t, amp)), amp / modAmpMult, counter - 1)
+        : t;
+    },
+    [ampLimit, modAmpMult]
+  );
+
+  const playSynth = useCallback(() => {
+    console.log("playing synth");
     const core = new WebRenderer();
 
     core.on("load", function () {
@@ -33,20 +49,40 @@ function App() {
     });
 
     (async function main() {
-      let node = await core.initialize(ctx, {
-        numberOfInputs: 0,
-        numberOfOutputs: 1,
-        outputChannelCount: [2],
-      });
+      if (audioContextRef.current) {
+        let node = await core.initialize(audioContextRef?.current, {
+          numberOfInputs: 0,
+          numberOfOutputs: 1,
+          outputChannelCount: [2],
+        });
 
-      node.connect(ctx.destination);
+        node.connect(audioContextRef.current.destination);
+      }
     })();
-  }
+  }, [modAmp, steps, startAmp, startFreq, recursiveFM]);
+
+  const togglePlay = () => {
+    if (audioContextRef?.current) {
+      if (playing) {
+        audioContextRef.current.suspend();
+      } else {
+        audioContextRef.current.resume();
+      }
+      setPlaying((play) => !play);
+      playSynth();
+    }
+  };
+
+  useEffect(() => {
+    playSynth();
+  }, [modAmp, steps, startAmp, startFreq, playSynth]);
 
   return (
     <div className="App">
-      <h2>start</h2>
-      <button onClick={playAudio}>PLAY AUDIO</button>
+      <h1>Recursive FM Synthesis</h1>
+      <button onClick={togglePlay}>
+        <span>{playing ? "Pause" : "Play"}</span>
+      </button>
       <h2>steps</h2>
       <input
         width="1000px"
